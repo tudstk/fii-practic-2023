e8c6233251b3c30269a9bd898024589cc2b3bc9c
DianaTerchea <44743846+DianaTerchea@users.noreply.github.com>
2023-03-06 11:07:06 +0200

    Session 1 - Setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLeYV0RtXJpUH5w4C4ukdJEQJbDiIWRk4",
+  authDomain: "fii-practic-memes.firebaseapp.com",
+  projectId: "fii-practic-memes",
+  storageBucket: "fii-practic-memes.appspot.com",
+  messagingSenderId: "429868905828",
+  appId: "1:429868905828:web:ffb3447a7ecbc69b9b7671",
+};
